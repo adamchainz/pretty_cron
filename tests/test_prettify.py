d0d4df91a22639a260925aa46318d1d27665adfd
Adam Chainz <adam@adamj.eu>
2015-07-28 07:15:51 +0100

    pytest-style plain asserts

--- tests/test_prettify.py
+++ tests/test_prettify.py
@@ -5,63 +5,64 @@ from pretty_cron import prettify
 
 class PrettyCronTest(unittest.TestCase):
     def test_yearly(self):
-        self.assertEqual(prettify("0 0 1 1 *"),
-                         "At 00:00 on the 1st of January")
+        assert prettify("0 0 1 1 *") == "At 00:00 on the 1st of January"
 
     def test_one_day_in_month(self):
-        self.assertEqual(prettify("0 0 1 * *"),
-                         "At 00:00 on the 1st of every month")
+        assert prettify("0 0 1 * *") == "At 00:00 on the 1st of every month"
 
     def test_one_day_in_month_2nd(self):
-        self.assertEqual(prettify("0 0 2 * *"),
-                         "At 00:00 on the 2nd of every month")
+        assert prettify("0 0 2 * *") == "At 00:00 on the 2nd of every month"
 
     def test_one_day_in_month_11th(self):
-        self.assertEqual(prettify("0 0 11 * *"),
-                         "At 00:00 on the 11th of every month")
+        assert prettify("0 0 11 * *") == "At 00:00 on the 11th of every month"
 
     def test_one_day_in_month_21st(self):
-        self.assertEqual(prettify("0 0 21 * *"),
-                         "At 00:00 on the 21st of every month")
+        assert prettify("0 0 21 * *") == "At 00:00 on the 21st of every month"
 
     def test_every_day_in_month(self):
-        self.assertEqual(prettify("12 15 * 1 *"),
-                         "At 15:12 every day in January")
+        assert prettify("12 15 * 1 *") == "At 15:12 every day in January"
 
     def test_every_specific_day_in_month(self):
-        self.assertEqual(prettify("0 0 * 1 1"),
-                         "At 00:00 on every Monday in January")
+        assert prettify("0 0 * 1 1") == "At 00:00 on every Monday in January"
 
     def test_weekly(self):
-        self.assertEqual(prettify("0 0 * * 0"),
-                         "At 00:00 every Sunday")
+        assert prettify("0 0 * * 0") == "At 00:00 every Sunday"
 
     def test_monthly_and_weekly(self):
-        self.assertEqual(prettify("0 0 1 * 1"),
-                         "At 00:00 on the 1st of every month and every Monday")
+        assert (
+            prettify("0 0 1 * 1") ==
+            "At 00:00 on the 1st of every month and every Monday"
+        )
 
     def test_every_specific_day_in_month_and_weekly(self):
-        self.assertEqual(
-            prettify("0 0 1 1 1"),
+        assert (
+            prettify("0 0 1 1 1") ==
             "At 00:00 on the 1st of January and on every Monday in January"
         )
 
     def test_daily(self):
-        self.assertEqual(prettify("0 0 * * *"), "At 00:00 every day")
+        assert prettify("0 0 * * *") == "At 00:00 every day"
 
     def test_hourly(self):
-        self.assertEqual(prettify("0 * * * *"),
-                         "At 0 minutes past every hour of every day")
+        assert (
+            prettify("0 * * * *") ==
+            "At 0 minutes past every hour of every day"
+        )
 
     def test_minutely(self):
-        self.assertEqual(prettify("* 5 * * *"),
-                         "Every minute between 05:00 and 05:59 every day")
+        assert (
+            prettify("* 5 * * *") ==
+            "Every minute between 05:00 and 05:59 every day"
+        )
 
     def test_continuous(self):
-        self.assertEqual(prettify("* * * * *"), "Every minute of every day")
+        assert prettify("* * * * *") == "Every minute of every day"
 
     def test_unsupported(self):
-        self.assertEqual(prettify("* */6 * * *"), "* */6 * * *")
+        assert prettify("* */6 * * *") == "* */6 * * *"
 
-    def test_invalid(self):
-        self.assertEqual(prettify('* * * * * *'), '* * * * * *')
+    def test_invalid_unchanged(self):
+        assert prettify('* * * * * *') == '* * * * * *'
+
+    def test_nonsense_unchanged(self):
+        assert prettify('Lalalala') == 'Lalalala'
